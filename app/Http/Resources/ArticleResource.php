664a4e0d2652e319b6f0f765a895349e1af72bd8
torabi.mohammad <torabi.mohammad@foodmarkethub.com>
2025-10-22 16:58:34 +0200

    add article resource

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'url' => $this->url,
+            'body' => $this->body,
+            'source' => $this->source,
+            'author' => $this->author,
+            'image_url' => $this->image_url,
+            'category' => $this->category,
+            'published_at' => $this->published_at?->toISOString(),
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
