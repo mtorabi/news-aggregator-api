4f3139ca7bff74989d600ef6e64b4e58725069c1
torabi.mohammad <torabi.mohammad@foodmarkethub.com>
2025-10-22 11:52:26 +0200

    add scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule the news fetch command to run daily at 6:00 AM
+Schedule::command('news:fetch')
+    ->dailyAt('06:00')
+    ->withoutOverlapping()
+    ->runInBackground()
+    ->timezone('UTC')
+    ->appendOutputTo(storage_path('logs/scheduler.log'));
